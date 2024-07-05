b99bf6777a6795fce79264418dd1979cf5b69334
ProgrammingPerson <mohamed.algraiw@gmail.com>
2024-07-05 18:46:59 -0500

    Removed unnecessary files and worked on model.
    Co-authored-by: Jolly06 <Jolly06@users.noreply.github.com>

--- data/data_prep.py
+++ data/data_prep.py
@@ -1,22 +1,27 @@
+import numpy as np
+from data_collection import get_data
+
 def getTrainingX():
+    df, train_data, test_data = get_data()
+    x_train=[]
+    y_train = []
+
+    #60 day look back for the training model
+    for i in range(60, len(train_data['scaled_values'])):
+        x_train.append(train_data['scaled_values'][i-60:i])
+        y_train.append(train_data['scaled_values'][i])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    #LSTM model as 3d array
+    x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+
+    x_test=[]
+    y_test = test_data['scaled_values']
+
+    for i in range(60, len(test_data)):
+        x_test.append(test_data['scaled_values'][i-60:i])
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
+
     return x_train, y_train
- 
-
-x_train=[]
-y_train = []
-
-for i in range(60, len(train_data['scaled_values'])):
-    x_train.append(train_data['scaled_values'][i-60:i])
-    y_train.append(train_data['scaled_values'][i])
-
-x_train, y_train = np.array(x_train), np.array(y_train)
-
-x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
-
-x_test=[]
-y_test = test_data['scaled_values']
-
-for i in range(60, len(test_data)):
-    x_test.append(test_data['scaled_values'][i-60:i])
-x_test = np.array(x_test)
-x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
--- src/model.py
+++ src/model.py
@@ -1,11 +1,32 @@
+# Imports
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
-
-# Imports 
 import sys
+sys.path.insert(0, '/Users/Mohamed/OneDrive/Documents/GitHub/stock-predictor/data')
+from data_prep import getTrainingX
 
+# Training data initialization
+x_train, y_train = getTrainingX()
 
-
+# Model initialization
 model = Sequential()
 
+# LSTM Layer 1
 model.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+model.add(Dropout(0.25))
+
+# LSTM Layer 2
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.25))
+
+# LSTM Layer 3
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.25))
+
+# LSTM Layer 2
+model.add(LSTM(units = 50))
+model.add(Dropout(0.25))
+
+# Final Layer
+model.add(Dense(units = 1))
+model.summary()
